e6e2103f5a08262828db4a3cc290d3d3c3f59ae1
jielian <jielian@jieliandeMacBook-Pro.local>
2015-03-30 09:23:41 +0800

    Initial Commit

---
+++ JLPay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JLPay
+//
+//  Created by jielian on 15/3/30.
+//  Copyright (c) 2015年 ShenzhenJielian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
